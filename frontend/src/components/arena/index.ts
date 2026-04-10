f17d93479d19f050e611c7e07ffb8abc52d0f4e3
mspeicher <m.speicher@prosoftclinical.com>
2026-04-10 15:41:36 -0400

    feat(arena): add barrel export for arena components

---
+++ frontend/src/components/arena/index.ts
@@ -0,0 +1,10 @@
+export { AgentPriceCard } from './AgentPriceCard';
+export type { AgentPriceCardProps } from './AgentPriceCard';
+
+export { PropBetUI } from './PropBetUI';
+export type { PropBetUIProps } from './PropBetUI';
+
+export { ArenaView } from './ArenaView';
+
+export { default as ArenaMatchStream } from './ArenaMatchStream';
+export { default as ArenaBettingForm } from './ArenaBettingForm';
